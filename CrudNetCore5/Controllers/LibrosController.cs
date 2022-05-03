3deec50e7234ad4209bacf82e4ba42127a06b2d7
Carlos Vargas <cavu93@hotmail.com>
2022-05-03 08:34:39 -0600

    Proyecto MVC ASP Core 5,Code First

---
+++ CrudNetCore5/Controllers/LibrosController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+
+namespace CrudNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+
+        //http get Index
+        public IActionResult Index()
+        {
+
+            IEnumerable<Libro> listLibros = _context.Libro;
+            return View(listLibros);
+        }
+
+
+        //http get create
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Create(Libro libro )
+        {
+            if (ModelState.IsValid) {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+             }
+            
+            //obtengo el libro
+            var libro=_context.Libro.Find(id);
+
+
+            if (libro==null)
+
+            {
+                return NotFound();
+            }
+            return View(libro);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]//valida no bots
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+
+
+
+
+        
+        public IActionResult Delete(int? id )
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //obtengo el libro
+            var libro = _context.Libro.Find(id);
+
+
+            if (libro == null)
+
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        [HttpDelete]
+        //[HttpPost]
+        [ValidateAntiForgeryToken]//valida no bots
+        public IActionResult DeleteLibro(int? id) {
+
+            var libro = _context.Libro.Find(id);
+            if (libro==null)
+            {
+                return NotFound();
+            }
+
+            _context.Libro.Remove(libro);
+            this._context.SaveChanges();
+            TempData["mensaje"] = "El libro se ha eliminado correctamente";
+            
+
+            return RedirectToAction("Index");
+        }
+
+        
+
+
+
+    }
+}
---
+++ CrudNetCore5/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudNetCore5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNetCore5.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Libro> Libro { get; set; }
+
+
+    }
+}
